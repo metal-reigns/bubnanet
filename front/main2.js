ca5a4af4ba1458be007df371daf4290e67428f11
metal-reigns <isniper19931221@yahoo.com>
2022-02-27 14:18:23 +0300

    update

--- front/main2.js
+++ front/main2.js
@@ -8,3 +8,22 @@ function oddOrEven(array) {
   console.log(oddOrEven([1, 2]));
   console.log(oddOrEven([1, 3]));
   console.log(oddOrEven([]));
+
+  // True, если строка 1 может преобразоваться в строку 2
+
+  function scramble(str1, str2) {
+    var count = Object.create(null);
+
+    Array.prototype.forEach.call(str1, function(a) {
+        count[a] = (count[a] || 0) + 1;
+    });
+
+    return Array.prototype.every.call(str2, function(a) {
+        return count[a]--;
+    });
+}
+
+console.log(scramble('rkqodlw', 'world'));              // true
+console.log(scramble('cedewaraaossoqqyt', 'codewars')); // true
+console.log(scramble('katas', 'steak'));                // false
+console.log(scramble('', 'o'));                // false
