d8265d3c1f0bc9c1fc4dae764860778e70526385
metal-reigns <isniper19931221@yahoo.com>
2022-04-16 20:49:24 +0300

    finish

--- front/main1.js
+++ front/main1.js
@@ -929,11 +929,46 @@ const userInfo = ({ name, commentsQty}) => {
                 .then(data => console.log(data))
                 .catch(error => console.log(error.message))
 
+        // Async function
 
+        const asyncFn = async () => {
+            throw new Error('There was an error')
+        }
 
+        asyncFn()
+            .then(value => console.log(value))
+            .catch(error => console.log(error.message))
 
+        // AWAIT
 
+        const timerPromise = () =>
+            new Promise ((resolve, reject) =>
+                setTimeout(() => resolve(), 2000))
 
+        const asyncFn = async () => {
+            console.log('Timer starts')
+            const startTime = performance.now()
+            await timerPromise()
+            const endTime = performance.now()
+            console.log('Timer ended', endTime - startTime)
+        }
 
+        asyncFn()
 
-    
+        // ASYNC / AWAIT
+
+        const getData = async (url) => {
+            const res = await fetch(url)
+            const json = await res.json()
+            return json
+        }
+
+        const url = 'https://jsonplaceholder.typicode.com/todos'
+
+        try{
+            const data = await getData(url)
+            console.log(data)
+        } catch (error) {
+            console.log(error.message)
+        }
+        
