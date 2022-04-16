96f054be38dacc2e9c0a220f70da337e30eb5195
metal-reigns <isniper19931221@yahoo.com>
2022-04-16 19:57:55 +0300

    async

--- front/main1.js
+++ front/main1.js
@@ -910,17 +910,24 @@ const userInfo = ({ name, commentsQty}) => {
     })
 
     fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-            console.log(response)
-            return response.json()
-        })
+        .then(response => response.json())
         .then(json => console.log(json))
         .catch(error => console.error(error))
 
 
+        //
 
+        const getData = (url) =>
+            new Promise((resolve, reject) =>
+            fetch(url)
+            .then(response => response.json())
+            .then(json => resolve(json))
+            .catch(error => reject(error))
+            )
 
-
+            getData('https://jsonplaceholder.typicode.com/todos')
+                .then(data => console.log(data))
+                .catch(error => console.log(error.message))
 
 
 
